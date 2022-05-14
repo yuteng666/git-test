2da727ea7c8263083ad54968d7b2bb764f50fe0b
yuteng <861836114@qq.com>
2022-05-14 09:34:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Description:
+ * @Author:yuteng
+ * @CreateTime:2022-05-14 9:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
