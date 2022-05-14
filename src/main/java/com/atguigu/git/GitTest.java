6b94d6deb19cdf22190ba2d3c2090deb27b43c99
yuteng <861836114@qq.com>
2022-05-14 10:19:03 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix!");
         System.out.println("hot-fix test!");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
